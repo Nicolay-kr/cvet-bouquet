17fe831aa8210a8c21499496bb532d2b40788164
Mikalai Kryshchanovich <Mikalai_Kryshchanovich@epam.com>
2022-10-03 09:40:13 +0300

    feat: add footer

--- cvet-bouquet-fe/src/components/Footer.js
+++ cvet-bouquet-fe/src/components/Footer.js
@@ -1,9 +1,351 @@
-import React from 'react'
+import React from 'react';
+import styles from '../../styles/Footer.module.css';
+import { Box, Typography } from '../../node_modules/@mui/material/index';
+import Image from 'next/future/image';
+import { useRouter } from 'next/router';
+import Link from '../Link';
+import logoFlower from '../assets/images/logo_flower_white.svg';
+import logoText from '../assets/images/logo_text_white.svg';
+import visa from '../assets/icons/visa.svg';
+import mastercard from '../assets/icons/mastercard.svg';
+import belcard from '../assets/icons/belcard.svg';
+import telegram from '../assets/icons/telegram.svg';
+import viber from '../assets/icons/viber.svg';
+import letter from '../assets/icons/letter.svg';
+import phone from '../assets/icons/phone.svg';
+import insta_mini from '../assets/icons/insta_mini.svg';
+import whatsapp from '../assets/icons/whatsapp.svg';
+import IconButton from '@mui/material/IconButton';
 
 export default function Footer() {
+  const router = useRouter();
   return (
     <>
-      <footer></footer>
+      <Box
+        width='100%'
+        px='10%'
+        pt={12.5}
+        pb={10}
+        bgcolor='secondary.main'
+        component='footer'
+      >
+        <Box className={styles.content}>
+          <Box className={styles.links}>
+            <Box className={styles.doubleColumn}>
+              <Typography
+                variant='subtitle1'
+                component='ul'
+                color='white'
+                fontWeight='700'
+                className={styles.navigation}
+              >
+                Меню
+                <Typography
+                  variant='subtitle1'
+                  component='li'
+                  color='white'
+                  onClick={() => router.push('/catalog')}
+                >
+                  Каталог
+                </Typography>
+                <Typography
+                  variant='subtitle1'
+                  component='li'
+                  color='white'
+                  onClick={() => router.push('/delivery')}
+                >
+                  Доставка и оплата
+                </Typography>
+                <Typography
+                  variant='subtitle1'
+                  component='li'
+                  color='white'
+                  onClick={() => router.push('/contacts')}
+                >
+                  Контакты
+                </Typography>
+                <Typography
+                  variant='subtitle1'
+                  component='li'
+                  color='white'
+                  onClick={() => router.push('/aboutus')}
+                >
+                  О нас
+                </Typography>
+              </Typography>
+              <Typography
+                variant='subtitle1'
+                component='ul'
+                color='white'
+                fontWeight='700'
+                className={styles.navigation}
+              >
+                Доставка и оплата
+                <Typography
+                  variant='subtitle1'
+                  component='li'
+                  color='white'
+                  onClick={() => router.push('/catalog')}
+                >
+                  Свободный платеж
+                </Typography>
+                <Typography
+                  variant='subtitle1'
+                  component='li'
+                  color='white'
+                  onClick={() => router.push('/delivery')}
+                >
+                  E-POS оплата
+                </Typography>
+                <Typography
+                  variant='subtitle1'
+                  component='li'
+                  color='white'
+                  onClick={() => router.push('/contacts')}
+                >
+                  Возврат денежных средств на карту
+                </Typography>
+                <Typography
+                  variant='subtitle1'
+                  component='li'
+                  color='white'
+                  onClick={() => router.push('/aboutus')}
+                >
+                  Доставка и самовывоз
+                </Typography>
+              </Typography>
+              <Typography
+                variant='subtitle1'
+                component='ul'
+                color='white'
+                fontWeight='700'
+                className={styles.navigation}
+              >
+                Полезное
+                <Typography
+                  variant='subtitle1'
+                  component='li'
+                  color='white'
+                  onClick={() => router.push('/catalog')}
+                >
+                  Каталог
+                </Typography>
+                <Typography
+                  variant='subtitle1'
+                  component='li'
+                  color='white'
+                  onClick={() => router.push('/delivery')}
+                >
+                  Доставка и оплата
+                </Typography>
+                <Typography
+                  variant='subtitle1'
+                  component='li'
+                  color='white'
+                  onClick={() => router.push('/contacts')}
+                >
+                  Контакты
+                </Typography>
+              </Typography>
+
+              <Box columnGap={1.5} className={styles.iconsConteiner}>
+                <IconButton component={Link} href='/'>
+                  <Image src={visa} alt='visa icon'></Image>
+                </IconButton>
+                <IconButton component={Link} href='/'>
+                  <Image src={mastercard} alt='mastercard icon'></Image>
+                </IconButton>
+                <IconButton component={Link} href='/'>
+                  <Image src={belcard} alt='belcard icon'></Image>
+                </IconButton>
+              </Box>
+              {/* </Box> */}
+            </Box>
+
+            <Box
+              className={styles.logoConteiner}
+              component={Link}
+              noLinkStyle
+              href='/'
+            >
+              <Image src={logoFlower} alt='logo flower'></Image>
+              <Image src={logoText} alt='logo flower'></Image>
+            </Box>
+            <Typography
+              variant='subtitle1'
+              component='ul'
+              color='white'
+              fontWeight='700'
+              className={styles.navigation}
+            >
+              Категории
+              <Typography
+                variant='subtitle1'
+                component='li'
+                color='white'
+                onClick={() => router.push('/catalog')}
+              >
+                Авторские букеты
+              </Typography>
+              <Typography
+                variant='subtitle1'
+                component='li'
+                color='white'
+                onClick={() => router.push('/delivery')}
+              >
+                В коробке
+              </Typography>
+              <Typography
+                variant='subtitle1'
+                component='li'
+                color='white'
+                onClick={() => router.push('/contacts')}
+              >
+                Монобукеты
+              </Typography>
+              <Typography
+                variant='subtitle1'
+                component='li'
+                color='white'
+                onClick={() => router.push('/aboutus')}
+              >
+                Корзины с цветами
+              </Typography>
+              <Typography
+                variant='subtitle1'
+                component='li'
+                color='white'
+                onClick={() => router.push('/aboutus')}
+              >
+                Цветочные письма
+              </Typography>
+              <Typography
+                variant='subtitle1'
+                component='li'
+                color='white'
+                onClick={() => router.push('/aboutus')}
+              >
+                Свадебная флористика
+              </Typography>
+            </Typography>
+            <Box className={styles.lastColumn}>
+              <Typography
+                variant='subtitle1'
+                component='ul'
+                color='white'
+                fontWeight='700'
+                className={styles.navigation}
+              >
+                Контакты
+                <Typography
+                  flex
+                  variant='subtitle1'
+                  component='li'
+                  onClick={() => router.push('/catalog')}
+                >
+                  <Image src={phone} alt='phone icon'></Image>
+                  <Typography
+                    ml={4}
+                    variant='subtitle1'
+                    component='p'
+                    color='white'
+                  >
+                    +375 44 556-55-55
+                  </Typography>
+                </Typography>
+                <Typography
+                  variant='subtitle1'
+                  component='li'
+                  color='white'
+                  onClick={() => router.push('/delivery')}
+                >
+                  <Image src={insta_mini} alt='insta icon'></Image>
+                  <Typography
+                    ml={4}
+                    variant='subtitle1'
+                    component='p'
+                    color='white'
+                  >
+                    @cvetbuket.by
+                  </Typography>
+                </Typography>
+                <Typography
+                  variant='subtitle1'
+                  component='li'
+                  color='white'
+                  onClick={() => router.push('/contacts')}
+                >
+                  <Image src={letter} alt='letter icon'></Image>
+                  <Typography
+                    ml={4}
+                    variant='subtitle1'
+                    component='p'
+                    color='white'
+                  >
+                    info@cvetbuket.by
+                  </Typography>
+                </Typography>
+              </Typography>
+              <Typography
+                variant='subtitle1'
+                component='ul'
+                color='white'
+                fontWeight='700'
+                className={styles.navigation}
+              >
+                Режим работы:
+                <Typography
+                  variant='subtitle1'
+                  component='li'
+                  color='white'
+                  onClick={() => router.push('/catalog')}
+                >
+                  Пн-Вс 09:00-21:00
+                </Typography>
+                <Typography
+                  variant='subtitle1'
+                  component='li'
+                  color='white'
+                  onClick={() => router.push('/delivery')}
+                >
+                  пр.Победителей, 27 пом. 344
+                </Typography>
+                <Typography
+                  variant='subtitle1'
+                  component='li'
+                  color='white'
+                  onClick={() => router.push('/contacts')}
+                >
+                  пр. Независимости, 104
+                </Typography>
+              </Typography>
+              <Box columnGap={1.5} className={styles.iconsConteiner}>
+                <IconButton component={Link} href='/'>
+                  <Image src={telegram} alt='telegram icon'></Image>
+                </IconButton>
+                <IconButton component={Link} href='/'>
+                  <Image src={viber} alt='viber icon'></Image>
+                </IconButton>
+                <IconButton component={Link} href='/'>
+                  <Image src={whatsapp} alt='whatsapp icon'></Image>
+                </IconButton>
+              </Box>
+            </Box>
+          </Box>
+          <Box className={styles.info}>
+            <Box className={styles.infoContent}>
+              <Typography variant='subtitle1' component='p' color='white'>
+                ИП Новицкая Н.В. УНП 193193419 Свидетельство о госрегистрации
+                выдано Минским горисполкомом 17.01.2019г.
+              </Typography>
+              <Typography variant='subtitle1' component='p' color='white'>
+                Регистрационный номер в торговом реестре РБ 455583 от
+                18.07.2019г.
+              </Typography>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
     </>
-  )
+  );
 }
--- cvet-bouquet-fe/src/theme.js
+++ cvet-bouquet-fe/src/theme.js
@@ -1,8 +1,26 @@
 import { createTheme } from '@mui/material/styles';
 import { red } from '@mui/material/colors';
 
+export const customFontsSize = {
+  xl: 2,
+  widthMobile: 0.875,
+  h1: { regular: 56, adaptiv: '2.91vw' },
+  h2: { regular: 28, adaptiv: '1.4vw' },
+  h3: { regular: 24, adaptiv: '1.25vw' },
+  h4: { regular: 20, adaptiv: '1.04vw' },
+  h5: { regular: 19, adaptiv: '0.94vw' },
+  h6: { regular: 17, adaptiv: '0.86vw' },
+  body1: { regular: 16, adaptiv: '0.83vw' },
+  body2: { regular: 12, adaptiv: '0.63vw' },
+  subtitle1: { regular: 18, adaptiv: '0.86vw' }, //links
+  subtitle2: { regular: 14, adaptiv: '0.73vw' },
+  button: { regular: 14, adaptiv: '0.73vw' },
+  caption: { regular: 30, adaptiv: '1.56vw' },
+};
+
 // Create a theme instance.
 const theme = createTheme({
+  // spacing: 4,
   breakpoints: {
     values: {
       xs: 0,
@@ -17,11 +35,179 @@ const theme = createTheme({
       main: '#746449',
     },
     secondary: {
-      main: '#19857b',
+      main: '#463D2F',
     },
     error: {
       main: red.A400,
     },
+  },
+  typography: {
+    fontFamily: [
+      'Raleway',
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+
+    h1: {
+      fontSize: customFontsSize.h1.regular,
+      color: 'white',
+      lineHeight: 1.1,
+      letterSpacing: '0.02em',
+      fontWeight: 700,
+      fontStyle: 'normal',
+      '@media (min-width:1921px)': {
+        fontSize: customFontsSize.h1.adaptiv,
+      },
+      '@media (max-width:1200px)': {
+        // fontSize: customFontsSize.h1 * customFontsSize.widthMobile,
+      },
+    },
+    h2: {
+      fontSize: customFontsSize.h2.regular,
+      lineHeight: 1.2,
+      fontWeight: 600,
+      textTransform: 'uppercase',
+      letterSpacing: '0.015em',
+      fontStyle: 'normal',
+      color: '#4F4F4F',
+      '@media (min-width:1921px)': {
+        fontSize: customFontsSize.h2.adaptiv,
+      },
+      '@media (max-width:600px)': {
+        fontSize: 24,
+        textAlign: 'end',
+      },
+    },
+    h3: {
+      fontSize: customFontsSize.h3.regular,
+      lineHeight: 1.4,
+      fontWeight: 400,
+      fontStyle: 'normal',
+      color: '#4F4F4F',
+      '@media (min-width:1921px)': {
+        fontSize: customFontsSize.h3.adaptiv,
+      },
+    },
+    h4: {
+      fontSize: customFontsSize.h4.regular,
+      lineHeight: 1.4,
+      fontWeight: 500,
+      fontStyle: 'normal',
+      textTransform: 'uppercase',
+      color: '#333333',
+      fontFamily: ['Montserrat', 'sans-serif'].join(','),
+      '@media (min-width:1921px)': {
+        fontSize: customFontsSize.h4.adaptiv,
+      },
+      '@media (max-width:600px)': {
+        fontSize: '18px',
+      },
+    },
+
+    h5: {
+      fontSize: customFontsSize.h5.regular,
+      lineHeight: 1.4,
+      fontWeight: 500,
+      fontStyle: 'normal',
+      textTransform: 'uppercase',
+      color: '#333',
+      '@media (min-width:1921px)': {
+        fontSize: customFontsSize.h5.adaptiv,
+      },
+    },
+    h6: {
+      fontSize: customFontsSize.h6.regular,
+      lineHeight: 1.4,
+      fontWeight: 600,
+      fontStyle: 'normal',
+      textTransform: 'uppercase',
+      color: '#4F4F4F',
+      scrollbarWidth: 'none',
+      '@media (min-width:1921px)': {
+        fontSize: customFontsSize.h6.adaptiv,
+      },
+    },
+    body1: {
+      fontSize: customFontsSize.body1.regular,
+      lineHeight: 1.4,
+      letterSpacing: '0.015em',
+      fontWeight: 400,
+      fontStyle: 'normal',
+      color: '#4F4F4F',
+      scrollbarWidth: 'none',
+      fontFamily: ['Montserrat', 'sans-serif'].join(','),
+      '@media (min-width:1921px)': {
+        fontSize: customFontsSize.body1.adaptiv,
+      },
+      '@media (max-width:480px)': {
+        fontSize: customFontsSize.body1.regular * customFontsSize.widthMobile,
+      },
+    },
+    body2: {
+      fontSize: customFontsSize.body2.regular,
+      lineHeight: 1.4,
+      fontWeight: 400,
+      fontStyle: 'normal',
+      color: '#7C7C7C',
+      fontFamily: ['Montserrat', 'sans-serif'].join(','),
+      '@media (min-width:1921px)': {
+        fontSize: customFontsSize.body2.adaptiv,
+      },
+    },
+    subtitle1: {
+      fontSize: customFontsSize.subtitle1.regular,
+      lineHeight: 1.4,
+      fontWeight: 500,
+      fontStyle: 'normal',
+      color: '#000',
+      fontFamily: ['Raleway', 'sans-serif'].join(','),
+      '@media (min-width:1921px)': {
+        fontSize: customFontsSize.subtitle1.adaptiv,
+      },
+    },
+    subtitle2: {
+      fontSize: customFontsSize.subtitle2.regular,
+      lineHeight: 1.4,
+      fontWeight: 600,
+      fontStyle: 'normal',
+      textTransform: 'uppercase',
+      color: '#333333',
+      '@media (min-width:1921px)': {
+        fontSize: customFontsSize.subtitle2.adaptiv,
+      },
+    },
+    button: {
+      fontSize: customFontsSize.button.regular,
+      lineHeight: 1.4,
+      fontWeight: 500,
+      fontStyle: 'normal',
+      textTransform: 'uppercase',
+      color: '#333333',
+      fontFamily: ['Montserrat', 'sans-serif'].join(','),
+      '@media (min-width:1921px)': {
+        fontSize: customFontsSize.button.adaptiv,
+      },
+    },
+    caption: {
+      fontSize: customFontsSize.caption.regular,
+      lineHeight: 1.4,
+      fontWeight: 600,
+      fontStyle: 'normal',
+      textTransform: 'uppercase',
+      color: '#333333',
+      '@media (min-width:1921px)': {
+        fontSize: customFontsSize.caption.adaptiv,
+      },
+    },
+    srOnly: {},
   },
   
 });
