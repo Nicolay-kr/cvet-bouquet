fc54e0abddb12980db5a6afe79cedf32388779f2
Mikalai Kryshchanovich <Mikalai_Kryshchanovich@epam.com>
2022-11-11 18:50:46 +0300

    feat: add main page to sanity

---
+++ cvet-bouquet-admin/schemas/pages/mainPage.js
@@ -0,0 +1,22 @@
+export default {
+  name: 'mainPage',
+  title: 'Главная страница',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Заголовок',
+      type: 'string',
+    },
+    {
+      name: 'subtitle',
+      title: 'Подзаголовок',
+      type: 'string',
+    },
+  ],
+  // preview: {
+  //   select: {
+  //     title: 'title',
+  //   },
+  // },
+};
