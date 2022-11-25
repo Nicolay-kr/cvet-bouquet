5d145b48d15e012bf7ebad2e561764c204a60766
Mikalai Kryshchanovich <Mikalai_Kryshchanovich@epam.com>
2022-11-25 10:20:37 +0400

    feat: add outline to active image

--- cvet-bouquet-fe/src/components/bouquePage/BouquetPage.js
+++ cvet-bouquet-fe/src/components/bouquePage/BouquetPage.js
@@ -101,7 +101,7 @@ export const Bouquet = ({bouquet,breadCrumbsList,instagramPosts}) => {
               <TransitionGroup>
                 <CSSTransition
                   key={activeImg._key}
-                  in={activeImg}
+                  in={true}
                   appear={true}
                   timeout={1000}
                   classNames='fade'
@@ -131,6 +131,7 @@ export const Bouquet = ({bouquet,breadCrumbsList,instagramPosts}) => {
                   height: { xs: '24vw', lg: 'min(240px,12.5vw)' },
                   objectFit: 'cover',
                   cursor: 'pointer',
+                  outline: activeImg._key===image._key? '4px solid #8C7B5F':null,
                 }}
               >
                 {image && (
@@ -298,61 +299,5 @@ export const Bouquet = ({bouquet,breadCrumbsList,instagramPosts}) => {
     </>
   );
   };
-  
-
-  export const getServerSideProps = async (pageContext) => {
-    const boucketSlug = pageContext.query.boucketSlug;
-    const categorySlug = pageContext.query.slug;
-  
-    if (!boucketSlug) {
-      return {
-        notFound: true,
-      };
-    }
-  
-    const query = `*[ _type == "bouquet" && slug.current == "${boucketSlug}" ]{
-      _id,
-      title,
-      slug,
-      images,
-      price,
-      description,
-    }`;
-  
-    const queryCategory = `*[ _type == "category" && slug.current == "${categorySlug}"]
-    {
-      _id,
-      slug,
-      title,
-    }`;
-  
-    const resultCategory = await sanityClient.fetch(queryCategory);
-    const result = await sanityClient.fetch(query);
-    const bouquet = result[0];
-  
-    const instagramUrl = `https://graph.instagram.com/me/media?fields=id,caption,media_url,media_type&access_token=${process.env.INSTAGRAM_TOKEN}`;
-    const data = await fetch(instagramUrl);
-    const instagramPosts = await data.json();
-  
-    if (!result) {
-      return {
-        notFound: true,
-      };
-    } else {
-      return {
-        props: {
-          description: bouquet.description.ru,
-          title: bouquet.title.ru,
-          images: bouquet.images,
-          price: bouquet.price,
-          slug: bouquet.slug,
-          id: bouquet._id,
-          instagramPosts: instagramPosts ? instagramPosts : [],
-          category: resultCategory,
-        },
-      };
-    }
-  };
-  
 
 export default Bouquet;
