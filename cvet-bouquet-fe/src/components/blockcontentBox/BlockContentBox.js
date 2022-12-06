40682d14d655f4d019eaf8f0c5b63a5385156638
Mikalai Kryshchanovich <Mikalai_Kryshchanovich@epam.com>
2022-12-06 10:38:52 +0400

    feat: remove aspect ratio from form

--- cvet-bouquet-fe/src/components/blockcontentBox/BlockContentBox.js
+++ cvet-bouquet-fe/src/components/blockcontentBox/BlockContentBox.js
@@ -11,9 +11,9 @@ export default function BlockContentBox({ blocks,fs=20 }) {
           fontFamily: 'Raleway, serif',
           color:'#000000',
           fontSize: { xs: '16px', lg: `${fs}px`, xxl: '1.2vw' },
-          '@media (-webkit-min-device-pixel-ratio: 1.25)': {
-            fontSize: { xs: '16px', lg: `${fs/1.25}px`, xxl: '1.2vw' },
-          },
+          // '@media (-webkit-min-device-pixel-ratio: 1.25)': {
+          //   fontSize: { xs: '16px', lg: `${fs/1.25}px`, xxl: '1.2vw' },
+          // },
         },
         my: 'auto',
         '& li+li': {
