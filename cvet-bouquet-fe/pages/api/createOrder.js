862ca043b57999d01b43c429374be367533505f0
Mikalai Kryshchanovich <Mikalai_Kryshchanovich@epam.com>
2023-01-15 17:59:06 +0400

    fix: updateOrderStatus

--- cvet-bouquet-fe/pages/api/createOrder.js
+++ cvet-bouquet-fe/pages/api/createOrder.js
@@ -7,15 +7,14 @@ import addOrder from '../../src/utils/sanityMethods/addOrder';
 import { sendMessageAboutOrder } from '../../src/utils/sendMessageAboutOrder';
 
 export default async function handler(req, res) {
-  // try {
-    const orderData = isJson(req.body)?JSON.parse(req.body):req.body;
-    const orderId = uniqid();
-    const orderNumber = generateOrderNumber(orderId);
-    orderData.id = orderId;
-    orderData.OrderNumber = orderNumber;
-    addOrder(orderData)
-    .then(()=>res.status(200).send('Order was created'))
-    .catch((e)=>res.status(500).send({ message: 'Order was not created',error: e}));
+  const orderData = isJson(req.body)?JSON.parse(req.body):req.body;
+  const orderId = uniqid();
+  const orderNumber = generateOrderNumber(orderId);
+  orderData.id = orderId;
+  orderData.OrderNumber = orderNumber;
+  try {
+    await addOrder(orderData)
+    return res.status(200).send({ message: 'Order was created'})
     // await sendMessageAboutOrder(orderData);
     // await createPayment({
     //     OrderNumber: orderData.orderNumber,
@@ -23,7 +22,8 @@ export default async function handler(req, res) {
     //     URL_RETURN_OK:'https://cvet-bouquet-nicolay-kr.vercel.app/cart',
     // });
     
-  // } catch (e) {
-  //   res.status(500).send({ message: 'Order was not created',error: e, });
-  // }
+  } catch (e) {
+    console.log(e);
+    return res.status(400).send({ message: 'Order was not created',error: e, });
+  }
 }
--- cvet-bouquet-fe/pages/api/updateOrderStatus.js
+++ cvet-bouquet-fe/pages/api/updateOrderStatus.js
@@ -1,22 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import isJson from '../../src/utils/isJson';
-import {updateOrder} from '../../src/utils/sanityMethods/updateOrder';
+import { updateOrder } from '../../src/utils/sanityMethods/updateOrder';
 
-
-export default function handler(req, res) {
-  // try{
-    const data = isJson(req.body)?JSON.parse(req.body):req.body;
+export default async function handler(req, res) {
+  try {
+    const data = isJson(req.body) ? JSON.parse(req.body) : req.body;
     const id = data.ordernumber.split('-').slice(2).join('');
     const status = data.orderstate;
-    if(id){
-      updateOrder(id, status)
-      .then(()=>res.status(200).send(`Success. Order with id ${id} was update`))
-      .catch(e=>res.status(500).send(`order wasn't update. Error: ${e}`))
-    }else{
-      res.status(500).send(`order with ${id} wasn't found.`)
+    if (id) {
+      await updateOrder(id, status);
+      return res.status(200).send(`Success. Order with id ${id} was update`);
+    } else {
+      return res.status(500).send(`order with ${id} wasn't found.`);
     }
-  // }catch(e){
-  //   res.status(500).send(`Order wasn't update. ${e}`)
-  // }
-
+  } catch (e) {
+    return res.status(500).send(`Order wasn't update. ${e}`);
+  }
 }
