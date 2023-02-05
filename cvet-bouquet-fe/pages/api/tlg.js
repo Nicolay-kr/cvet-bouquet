ab15c35fb0f1195192609ecc185608b2160d24fe
Mikalai Kryshchanovich <Mikalai_Kryshchanovich@epam.com>
2023-02-05 14:57:52 +0400

    feat: test tlg

--- cvet-bouquet-fe/pages/api/tlg.js
+++ cvet-bouquet-fe/pages/api/tlg.js
@@ -5,17 +5,19 @@ export default async (req, res) => {
   console.log(req.body)
   try{
     if (req.body.message.text === '/start') {
-      const message =
-        'Welcome to <i>NextJS News Channel</i> <b>' +
-        req.body.message.from.first_name +
-        'To get a list of commands sends /help';
+      console.log('inside start')
+      console.log(req.body.message.chat.id)
+      const message ='To get a list of commands sends /help';
+      console.log(message)
       const ret = await fetch(
         `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=${req.body.message.chat.id}&text=${message}`
       );
     }
     if (req.body.message.text === '/help') {
-      const message =
-        'Help for NextJS News Channel keyword to search for keyword in my Medium publication';
+      console.log('inside help')
+      console.log(req.body.message.chat.id)
+      const message ='Help for NextJS News Channel keyword to search for keyword in my Medium publication';
+      console.log(message)
       const ret = await fetch(
         `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=${req.body.message.chat.id}&text=${message}`
       );
