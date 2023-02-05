4ef9fa546e63b69d7268a0e300954b0935bffaae
Mikalai Kryshchanovich <Mikalai_Kryshchanovich@epam.com>
2023-02-05 14:47:55 +0400

    feat: update tlg

--- cvet-bouquet-fe/pages/api/tlg.js
+++ cvet-bouquet-fe/pages/api/tlg.js
@@ -8,16 +8,16 @@ export default async (req, res) => {
       const message =
         'Welcome to <i>NextJS News Channel</i> <b>' +
         req.body.message.from.first_name +
-        '</b>.%0ATo get a list of commands sends /help';
+        'To get a list of commands sends /help';
       const ret = await fetch(
-        `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=${req.body.message.chat.id}&text=${message}&parse_mode=HTML`
+        `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=${req.body.message.chat.id}&text=${message}`
       );
     }
     if (req.body.message.text === '/help') {
       const message =
-        'Help for <i>NextJS News Channel</i>.%0AUse /search <i>keyword</i> to search for <i>keyword</i> in my Medium publication';
+        'Help for NextJS News Channel keyword to search for keyword in my Medium publication';
       const ret = await fetch(
-        `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=${req.body.message.chat.id}&text=${message}&parse_mode=HTML`
+        `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=${req.body.message.chat.id}&text=${message}`
       );
     }
     res.status(200).send('OK');
