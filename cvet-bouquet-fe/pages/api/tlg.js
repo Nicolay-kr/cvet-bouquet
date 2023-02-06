89f19f0650146242848a81c509421846d90ea20a
Mikalai Kryshchanovich <Mikalai_Kryshchanovich@epam.com>
2023-02-06 17:34:54 +0400

    fix: order if it start from big syllable

--- cvet-bouquet-fe/pages/api/tlg.js
+++ cvet-bouquet-fe/pages/api/tlg.js
@@ -32,7 +32,7 @@ export default async (req, res) => {
         );
       }
     } else if (req.body.message?.text.match(/order-\d+$/gmi)?.length) {
-      const order = await getOrderStatus(req.body.message?.text, req.body.message.chat.id);
+      const order = await getOrderStatus(req.body.message?.text?.toLowerCase(), req.body.message.chat.id);
       if (order?.user?.chatId && order.user.chatId === `${req.body.message.chat.id}`) {
         const message = `
         Номер заказа: ${order.OrderNumber};
