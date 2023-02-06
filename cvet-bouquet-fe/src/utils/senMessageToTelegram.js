a2fb2bb75f4a78d1c5f89edff8c115feee6efcf0
Mikalai Kryshchanovich <Mikalai_Kryshchanovich@epam.com>
2023-02-06 17:31:40 +0400

    refactor: remove unnecesary console logs

--- cvet-bouquet-fe/src/utils/senMessageToTelegram.js
+++ cvet-bouquet-fe/src/utils/senMessageToTelegram.js
@@ -4,21 +4,18 @@ async function senMessageToTelegram(message) {
   const tgbot = process.env.TELEGRAM_TOKEN;
   try {
     const users = await getUsers();
-    console.log(users);
     const activeUsers = users.filter(
       (user) =>
         user.active &&
         user.telegramBlock.telegramAllow &&
         user.telegramBlock.chatId
     );
-    console.log(activeUsers);
     if (activeUsers.length) {
       const messages = activeUsers.map((user) =>
         fetch(
           `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=${user.telegramBlock.chatId}&text=${message}`
         )
       );
-      console.log(messages);
       return await Promise.all(messages);
     }
   } catch (e) {
