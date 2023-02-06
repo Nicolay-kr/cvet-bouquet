939f2fe9993786d7e874fd8e209a17d75b441f06
Mikalai Kryshchanovich <Mikalai_Kryshchanovich@epam.com>
2023-02-06 10:30:47 +0400

    feat: test tlgv2 with html

--- cvet-bouquet-fe/pages/api/tlg.js
+++ cvet-bouquet-fe/pages/api/tlg.js
@@ -1,40 +1,31 @@
+
 export default async (req, res) => {
+
   const tgbot = process.env.TELEGRAM_TOKEN;
-  console.log(req.body);
-  try {
+  try{
     if (req.body.message?.text === '/start') {
-      console.log('inside start');
-      console.log(req.body.message.chat.id);
-      const message = 'To get a list of commands sends /help';
-      console.log(message);
+      console.log('message',req.body.message?.text)
+      const message =
+        'Welcome to <i>NextJS News Channel</i> <b>' +
+        req.body.message.from.first_name +
+        '</b>.%0ATo get a list of commands sends /help';
       const ret = await fetch(
-        `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=${req.body.message.chat.id}&text=${message}`
+        `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=${req.body.message.chat.id}&text=${message}&parse_mode=HTML`
       );
     }
     if (req.body.message?.text === '/help') {
-      console.log('inside help');
-      console.log('TELEGRAM_TOKEN', process.env.TELEGRAM_TOKEN);
-      console.log(req.body.message.chat.id);
+      console.log('message',req.body.message?.text)
       const message =
-        'Help for NextJS News Channel keyword to search for keyword in my Medium publication';
-      console.log(message);
+        'Help for <i>NextJS News Channel</i>.%0AUse /search <i>keyword</i> to search for <i>keyword</i> in my Medium publication';
       const ret = await fetch(
-        `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=${req.body.message.chat.id}&text=${message}`
+        `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=${req.body.message.chat.id}&text=${message}&parse_mode=HTML`
       );
     }
-    // if (req.body.message?.text === '/test') {
-      console.log('TELEGRAM_TOKEN', process.env.TELEGRAM_TOKEN);
-      console.log(req.body.message.chat.id);
-      await fetch(
-        'https://api.telegram.org/bot6123262918:AAETDuStLhRBZUsvfKFh2HA-6KyalCDrKuw/sendMessage?chat_id=336711852&text=customtext'
-      )
-        // .then((response) => console.log('response: ', response))
-        // .catch((error) => console.log('error: ', error));
-    // }
     return res.status(200).send('OK');
-    // return res.status(200).send('OK');
-  } catch (e) {
-    console.log(e);
-    res.status(500).send(e);
+
+  }catch(e){
+    console.log(e)
+    return res.status(500).send(e);
   }
+
 };
